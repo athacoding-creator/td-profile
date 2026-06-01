66a64155be730c10e79cb890f74cf06b2879cbb5
Atha21 <athacoding@gmail.com>
2026-06-01 12:29:45 +0700

    text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        "josefin-sans": ["Josefin Sans", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
